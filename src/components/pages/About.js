6707d3b442fba47b03bf68e1444256419b042259
JCaloca <jacobcaloca1@gmail.com>
2023-02-13 09:44:14 -0800

    Typo fix and rewording

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React from 'react';
 
 export default function About() {
   return (
@@ -8,18 +8,18 @@ export default function About() {
         <hr />
         <img
           className="profile-pic col-md-6 float-md-end mb-3 ms-md-3"
-          src={process.env.PUBLIC_URL + "/assets/prof-pic.jpg"}
+          src={process.env.PUBLIC_URL + '/assets/prof-pic.jpg'}
           alt="J. Caloca"
         />
         <p className="lead">
-          Hello, I'm Jacob Caloca. A recent graduate from the UC Berkely Coding
+          Hello, I'm Jacob Caloca. A recent graduate from the UC Berkeley Coding
           Bootcamp Extension, with a background in customer service and sales.
           Throughout the bootcamp I have expanded my technical knowledge and
           widened my practical know how.
         </p>
         <p>
-          I am excited to continue to grow professionally as a new graduate and
-          expand my tech stack more.
+          I am looking to bring my attention to detail and techical skills to a
+          company with which I can grow and become a valuable asset.
         </p>
       </div>
     </div>
